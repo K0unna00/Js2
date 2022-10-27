969359293b826f48489916edf58c31d4f7aa5bc8
K0unna00 <78pf4jz@code.edu.az>
2022-10-27 12:56:54 +0400

    First

---
+++ Js2/main.js
@@ -0,0 +1,93 @@
+//// =========Task1=========
+// const a= +prompt("Enter the value");
+// for (let i = 1; i < 1000; i++) {
+//     if(i%a==0){
+//         console.log(i);
+//     }
+// }
+//// =========Task2=========
+// const a= +prompt("Enter the value");
+// for (let i = 0; i < Math.sqrt(a); i++) {
+//     if(i*i<a){
+//         console.log(i);
+//     }
+// }
+//// =========Task3=========
+// const a= +prompt("Enter the value");
+// if(a%2==0){
+//     console.log(a/2);
+// }
+// else{
+//     for (let i = Math.floor(a/2); i > 0 ; i--) {
+//         if(a%i==0){
+//             console.log(i);
+//             break;
+//         }
+//     }   
+// }
+//// =========Task4=========
+// const a = +prompt("Enter the value");
+// const b = +prompt("Enter the value");
+
+// for (let i = a; i <=b; i++) {
+//     if (i % 7 == 0) {
+//         console.log(i);
+//     }
+// }
+//// =========Task5=========
+// const a = +prompt("Enter the value");
+// let i=3;
+// let b=1;
+// let c=1;
+// let number=b+c;
+// while(i<a){
+//     i++;
+//     b=c;
+//     c=number;
+//     number=b+c;
+// }
+// console.log(number);
+//// =========Task6=========
+
+
+//I didnt undertstood how algoritm works
+
+
+
+//// =========Task7=========
+// let a = +prompt("Enter the value");
+// let b;
+// let c=0;
+// while(a!=0){
+//     b=a%10;
+//     c=c*10+b;
+//     a=Math.floor(a/10);
+// }
+// console.log(c);
+
+//// =========Task8=========
+// let a = +prompt("Enter the value");
+// let b = +prompt("Enter the value");
+// let c , d;
+// let check=false;
+// while(a!=0){
+//     c=a%10;
+//     a=Math.floor(a/10);
+//     while(b!=0){
+//         d=b%10;
+//         b=Math.floor(b/10);
+//         if(c==d){
+//             check=true;
+//             break;
+//         }
+//     }
+//     if(check){
+//         break;
+//     }
+// }
+// if(check){
+//     console.log("Yes");
+// }
+// else(console.log("No"));
+
+
